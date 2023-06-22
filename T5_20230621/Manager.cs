b8345fce7a55a8cb55324a5e9024185aa221f496
TrungNV2612 <128463356+TrungNV2612@users.noreply.github.com>
2023-06-22 13:25:31 +0700

    Add files via upload

---
+++ T5_20230621/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_20230621
+{
+    internal class Manager
+    {
+        private string Version = "1.00";
+        private string name = "Class Manager";
+        public Manager() 
+        { 
+            Console.WriteLine(this.name + " " + this.Version); 
+        }
+        public virtual void AddNew()
+        { 
+            Console.WriteLine("AddNew"); 
+        }
+        public virtual void Update()
+        {
+            Console.WriteLine("Update");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Delete");
+        }
+        public virtual void Find()
+        {
+            Console.WriteLine("Find");
+        }
+        public virtual void ViewAll()
+        {
+            Console.WriteLine("ViewAll");
+        }
+    }
+}
---
+++ T5_20230621/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_20230621
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int selected;
+            Employee_Manager manager=new Employee_Manager();
+            do
+            {
+                Console.WriteLine("***EMPLOYEE MANAGER***");
+                Console.WriteLine("1. Search Employee by Name or EmpNo");
+                Console.WriteLine("2. Add New Employee");
+                Console.WriteLine("3. Update Employee");
+                Console.WriteLine("4. Delete Employee");
+                Console.WriteLine("5. ViewAll Employee");
+                Console.WriteLine("6. Exit");
+                Console.Write("   Select (1-6): ");
+                selected = Convert.ToInt16(Console.ReadLine());
+                switch (selected) 
+                {
+                    case 1:
+                        Console.Clear();
+                        manager.Find();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        manager.AddNew();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        manager.Update();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        manager.Delete();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        manager.ViewAll();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("-------- END ---------");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+            while (selected != 6);
+
+        }
+    }
+}
