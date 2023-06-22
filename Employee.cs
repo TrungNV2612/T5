88130e6fa278f108154b6fb314a5c33fde878f90
M7540 <M7540@CNTT-TRUNGNV-LA>
2023-06-22 13:31:23 +0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_20230621
+{
+    internal class Employee
+    {
+        public String no;
+        public String name;
+        public String email;
+
+        //geter and constructor
+        public String GetNo()
+        {
+            return this.no;
+        }
+
+        public String GetName()
+        {
+            return this.name;
+        }
+
+        public String GetEmail()
+        {
+            return (this.email);
+        }
+
+        public void SetObj(Employee Enter)
+        {
+            this.no = Enter.no;
+            this.name = Enter.name;
+            this.email = Enter.email;
+        }
+
+        public Employee(string no, string name, string email)
+        {
+            this.no = no;
+            this.name = name;
+            this.email = email;
+        }
+
+        public override string ToString()
+        {
+            return no + ", " + name + ", " + email;
+        }
+
+    }
+}
---
+++ Employee_Manager.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_20230621
+{
+    internal class Employee_Manager:Manager
+    {
+        private string Version = "2.00";
+        private string name = "Employee_Manager";
+        public int MAX = 10;
+        private Employee[] employees;
+
+        public Employee_Manager() :base()
+        {
+            Console.WriteLine(this.name + " " + this.Version);
+            this.employees = new Employee[]{
+                new Employee("E001", "hoangnm", "hoangnm@gmail.com"),
+                new Employee("E002", "namph", "namph@gmail.com"),
+                new Employee("E003", "minhnv", "minhnv@gmail.com"),
+            };
+                
+        }
+        public override void AddNew()
+        {
+            int indexmax=employees.Length;            
+            Employee employeeenter = EnterArr();
+            Employee[] ArrTrunggian = employees;
+            employees = new Employee[indexmax + 1];
+            for(int i = 0; i <= indexmax; i++)
+            {
+                if ( i<indexmax) 
+                {
+                    employees[i] = ArrTrunggian[i];    
+                }
+                else
+                {
+                    employees[i] = employeeenter;
+                }                
+            }         
+            
+        }
+        public override void Delete()
+        {
+            Console.WriteLine("Enter EmpNo or Name: ");
+            string searchkey = Console.ReadLine();
+            for (int i = 0;i < employees.Length;i++) 
+            {
+                if (employees[i].GetNo().Equals(searchkey) || employees[i].GetName().Equals(searchkey))
+                {
+                    Employee[] arr_trunggian = employees;
+                    int indexmax = employees.Length;
+                    employees = new Employee[indexmax - 1];
+                    for (int j = 0; j < indexmax-1; j++)
+                    {
+                        if (j<i)
+                        {
+                            employees[j] = arr_trunggian[j];
+                        }
+                        else
+                        {
+                            employees[j] = arr_trunggian[j + 1];
+                        }
+                    }
+                }               
+
+            }
+
+
+        }
+        public override void Update()
+        {
+            Console.WriteLine("Enter EmpNo or Name: ");
+            String searchkey=Console.ReadLine();
+            Employee employeeenter = EnterArr();
+            foreach (Employee emp in employees) 
+            {
+                if ( emp.GetNo().Equals(searchkey) || emp.GetName().Equals(searchkey) )
+                {
+                    emp.SetObj(employeeenter);
+                }
+            }
+        }
+        public override void Find() 
+        {
+            Console.Write("Enter EmpNo or Name: ");
+            String searchKey = Console.ReadLine();            
+
+            // search
+            Employee[] result = new Employee[MAX];
+            int count = 0;
+            foreach (Employee emp in employees)
+            {
+                if (emp.GetNo().Equals(searchKey) || emp.GetName().Equals(searchKey))
+                {
+                    result[count++] = emp;
+                    //count++;
+                }
+            }
+
+            // print
+            if (count > 0)
+            {
+                PrintList(result);
+            }
+            else
+            {
+                Console.WriteLine("Not Found!");
+            }
+                        
+        }
+
+
+        private void PrintList(Employee[] arr)
+        {
+            foreach (Employee item in arr)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        private Employee EnterArr()
+        {
+            Console.WriteLine("Moi ban nhap Record ");
+            string no = Console.ReadLine();
+            string name= Console.ReadLine();
+            string  email= Console.ReadLine();
+            Employee EmployeeEnter = new Employee(no, name, email);
+            return EmployeeEnter;
+        }
+        public override void ViewAll()
+        {
+            PrintList(this.employees);
+        }
+    }
+}
